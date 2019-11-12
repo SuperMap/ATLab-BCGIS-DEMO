22488646a4cead2d371c0765d18961ef1a088e6c
stone-ch <great_cy_ang@163.com>
2019-11-12 12:16:07 +0800

    modify ports

--- DigitalCertWithACL-DEMO/web/js.js
+++ DigitalCertWithACL-DEMO/web/js.js
@@ -1,9 +1,9 @@
 // enroll
 
-var RESTURL = "http://127.0.0.1:4000";
-var FileURL = "http://127.0.0.1:4444";
-// var RESTURL = "http://175.154.161.50:7002";
-// var FileURL = "http://175.154.161.50:7001";
+// var RESTURL = "http://127.0.0.1:4000";
+// var FileURL = "http://127.0.0.1:4444";
+var RESTURL = "http://175.154.161.50:7002";
+var FileURL = "http://175.154.161.50:7001";
 
 $(document).ready(function () {
     // 设置预设值
