3a1ff94c938bde1f319619d405e264788f0c2a7f
Yang Cheng <great_cy_ang@163.com>
2019-11-28 22:10:37 +0800

    finish trace s3m show page

--- 3D-DEMO/js.js
+++ 3D-DEMO/js.js
@@ -1,6 +1,5 @@
-
 /**
- *  展示系统的界面设置   
+ *  展示系统的界面设置
  */
 
 
@@ -14,20 +13,132 @@ $(document).ready(function () {
         <li><a href=\"#\" id=\"info\"></a></li> \
         </ul>");
 
-    // 发布地图（时间为点击）
-    $("#publish_btn").click(function () {
-        var params = {
-            "workspaceName": document.getElementById("publish_workspace").value,
-            "datastoreName": document.getElementById("publish_datastore").value,
-            "featuretypeName": document.getElementById("publish_layer").value
-        };
+    $("#btn_show_s3m_moudle").click(function creatTab() {
         $.ajax({
-            type: 'post',
-            contentType: "application/json",
-            url: 'http://localhost:8899/bcgis/mapservice/wms/publish',
-            data: JSON.stringify(params),
+            type: 'get',
+            // contentType: "application/json",
+            url: 'http://172.16.15.66:8081/server_war_exploded/webapi/blockchain/history',
+            // data: JSON.stringify(params),
             success: function (data) {
-                console.log('data: ' + JSON.stringify(data));
+                let recoreds = JSON.parse(data);
+                var tab = '<table border=1 width="500" cellpadding="10" align="center">'
+                tab += "<caption>历史版本查询</caption>";
+                tab += "<tr><th>版本号</th>\
+                            <th>修改时间</th>"
+                for (var i = 0; i < recoreds.length; i++) {
+                    tab += '<tr>'
+                    for (var j = 0; j < 2; j++) {
+                        if (j == 0) {
+                            // target 当点击时在另外的页面打开
+
+                            tab += "<td ><label style=\"color:blue; text-align: center;\" class=\"showclass\" id=\"show" + i + j + "\">" + i + "</label></td>"
+                        } else {
+                            tab += "<td >" + recoreds[i]["Timestamp"] + "</td>"
+                        }
+                    }
+                    tab += '</tr>';
+                }
+                tab += '</table>';
+                $("#version_list").html(tab);
+            },
+            error: function (err) {
+                console.log('err: ');
+                console.log(JSON.stringify(err));
+            }
+        });
+    });
+
+    function show(mapid) {
+        //初始化viewer部件
+        $("#cesiumContainer").show();
+        $("#option").hide();
+
+        $.ajax({
+            type: 'get',
+            // contentType: "application/json",
+            url: 'http://172.16.15.66:8081/server_war_exploded/webapi/blockchain?modelID=model001',
+            // data: JSON.stringify(params),
+            success: function (data) {
+                console.log(data);
+                data = "{\"Records\":[" + data + "]}";
+                let recoreds = JSON.parse(data);
+                var viewer = new Cesium.Viewer('cesiumContainer');
+                var scene = viewer.scene;
+                var widget = viewer.cesiumWidget;
+                try {
+                    //打开所发布三维服务下的所有图层
+                    scene.camera.setView({
+                        destination: new Cesium.Cartesian3(-2172108.996759282, 4375350.277824589, 4101673.0066891187),
+                        orientation: {
+                            heading: 4.354565476048545,
+                            pitch: -0.35245897259115955,
+                            roll: 9.353513519272383e-10
+                        }
+                    });
+
+                    var s3mFilePath = recoreds["Records"];
+
+                    // 定义数据展示的位置
+                    var points = [116.395074412521, 40.0167102653286, 39.4119813283905];
+
+                    // 循环输出图层
+                    for (var i = 0; i < s3mFilePath.length; i++) {
+                        var exam = '../s3m/' + s3mFilePath[i];
+                        var keymap = {};
+                        keymap[exam] = [];
+                        var layer = new Cesium.DynamicLayer3D(scene._context, [exam]);
+                        layer.updateInterval = 500;//动态图层更新时间
+                        layer.enableLocalOffset = false;//禁止模型局部偏移
+                        scene.primitives.add(layer);
+                        var pillarState = new Cesium.DynamicObjectState({
+                            id: i,
+                            longitude: points[0],
+                            latitude: points[1],
+                            altitude: points[2],
+                            scale: new Cesium.Cartesian3(1, 1, 1)
+                        });
+                        keymap[exam].push(pillarState);
+                        for (var key in keymap) {
+                            layer.updateObjectWithModel(key, keymap[key]);
+                        }
+                    }
+                } catch (e) {
+                    if (widget._showRenderLoopErrors) {
+                        var title = '渲染时发生错误，已停止渲染。';
+                        widget.showErrorPanel(title, undefined, e);
+                    }
+                }
+            }
+        });
+    }
+
+    // $(document).click(function(e) { // 在页面任意位置点击而触发此事件
+    //     var v_id = $(e.target).attr('id');
+    // })
+
+    $('body').on('click' , '.showclass' , function(){
+        // 获取鼠标点击元素的 id
+        $(document).click(function(e) { // 在页面任意位置点击而触发此事件
+            var v_id = $(e.target).attr('id');
+            show($("#" + v_id).html().trim());
+            $(document).unbind("click");
+        })
+    });
+
+    // 发布地图（时间为点击）
+    $("#btn_saveFile").click(function () {
+        // var params = {
+        //     "workspaceName": document.getElementById("publish_workspace").value,
+        //     "datastoreName": document.getElementById("publish_datastore").value,
+        //     "featuretypeName": document.getElementById("publish_layer").value
+        // };
+        $.ajax({
+            type: 'get',
+            // contentType: "application/json",
+            url: 'http://172.16.15.66:8081/server_war_exploded/webapi/blockchain',
+            // data: JSON.stringify(params),
+            success: function (data) {
+                console.log('data: ' + data);
             },
             error: function (err) {
                 console.log('err: ');
@@ -39,7 +150,7 @@ $(document).ready(function () {
     $("#maplist_btn").click(function () {
         $.ajax({
             type: 'get',
-            url: 'http://localhost:8899/bcgis/mapservice/wms/list',
+            url: 'http://172.16.15.66:8899/bcgis/mapservice/wms/list',
             data: {
                 workspaceName: document.getElementById("maplist_workspace").value,
                 datastoreName: document.getElementById("maplist_datastore").value,
@@ -105,27 +216,27 @@ $(document).ready(function () {
     });
 
     // 出现 ajax 请求，将获取的路径返回给后端，让后端完成数据的存储过程，最后返回一个值表明数据存储完毕即可
-    $("#btn_saveFile").click(function () {
+    // $("#btn_saveFile").click(function () {
 
-        var params = document.getElementById("input_getFilePath").value;
+    //     // var params = document.getElementById("input_getFilePath").value;
 
-        // $.ajax({
-        //     type: 'post',
-        //     contentType: "application/json",
-        //     url: 'http://localhost:8899/bcgis/mapservice/wms/publish',
-        //     data: JSON.stringify(params),
-        //     success: function (data) {
-        //         console.log('data: ' + JSON.stringify(data));
-        //     },
-        //     error: function (err) {
-        //         console.log('err: ');
-        //         console.log(JSON.stringify(err));
-        //     }
-        // });
+    //     // $.ajax({
+    //     //     type: 'post',
+    //     //     contentType: "application/json",
+    //     //     url: 'http://localhost:8899/bcgis/mapservice/wms/publish',
+    //     //     data: JSON.stringify(params),
+    //     //     success: function (data) {
+    //     //         console.log('data: ' + JSON.stringify(data));
+    //     //     },
+    //     //     error: function (err) {
+    //     //         console.log('err: ');
+    //     //         console.log(JSON.stringify(err));
+    //     //     }
+    //     // });
 
-        alert("文件保存完毕，请点击返回")
+    //     alert("文件保存完毕，请点击返回")
 
-    });
+    // });
 
 });
 
