f6bfe2fb81f0c0e389231326e82245e1291df142
Yang Cheng <great_cy_ang@163.com>
2019-12-09 09:54:22 +0800

    uncomment hdfs

--- server/src/main/java/com/supermap/atlab/storage/Hdfs.java
+++ server/src/main/java/com/supermap/atlab/storage/Hdfs.java
@@ -1,48 +1,47 @@
-//package com.supermap.atlab.storage;
-//
-//import org.apache.commons.io.IOUtils;
-//import org.apache.hadoop.conf.Configuration;
-//import org.apache.hadoop.fs.FSDataOutputStream;
-//import org.apache.hadoop.fs.FileSystem;
-//
-//import java.io.IOException;
-//import java.io.InputStream;
-//import java.net.URI;
-//import java.util.logging.Logger;
-//
-//public class Hdfs {
-//
-//    private Logger logger = Logger.getLogger(Hdfs.class.toString());
-//    private final String ipAddress = "hdfs://172.16.15.65:9000";
-//    private final String hdfsStorePath = "/user/bcgis/";
-//    private FileSystem fs = null;
-//    private String userName = "java";
-//
-//    private FileSystem getFs() {
-//        Configuration conf = new Configuration();
-//        conf.set("bcgis",ipAddress);
-//        conf.set("dfs.replication", "3");
-//        try {
-//            fs = FileSystem.get(new URI(ipAddress), conf, userName);
-//        }catch (Exception e){
-//        }
-//        return fs;
-//    }
-//
-//    public String hdfsUploadFile(InputStream fileInputStream, String fileExtName, String hash)  {
-//        FileSystem fs = new Hdfs().getFs();
-//        String uploadPath = hdfsStorePath + hash + fileExtName;
-//        String destpath = ipAddress + uploadPath;
-//        org.apache.hadoop.fs.Path dst = new org.apache.hadoop.fs.Path(destpath);
-//        FSDataOutputStream os = null;
-//        try {
-//            os = fs.create(dst);
-//            IOUtils.copy(fileInputStream, os);
-//            fs.close();
-//        } catch (IOException e) {
-//            e.printStackTrace();
-//        }
-////        logger.info("sucessful upload file !");
-//        return  hash + fileExtName;
-//    }
-//}
+package com.supermap.atlab.storage;
+
+import org.apache.commons.io.IOUtils;
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.FSDataOutputStream;
+import org.apache.hadoop.fs.FileSystem;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URI;
+import java.util.logging.Logger;
+
+public class Hdfs {
+
+    private Logger logger = Logger.getLogger(Hdfs.class.toString());
+    private final String ipAddress = "hdfs://172.16.15.65:9000";
+    private final String hdfsStorePath = "/user/bcgis/";
+    private FileSystem fs = null;
+    private String userName = "java";
+
+    private FileSystem getFs() {
+        Configuration conf = new Configuration();
+        conf.set("bcgis",ipAddress);
+        conf.set("dfs.replication", "3");
+        try {
+            fs = FileSystem.get(new URI(ipAddress), conf, userName);
+        }catch (Exception e){
+        }
+        return fs;
+    }
+
+    public String hdfsUploadFile(InputStream fileInputStream, String fileExtName, String hash)  {
+        FileSystem fs = new Hdfs().getFs();
+        String uploadPath = hdfsStorePath + hash + fileExtName;
+        String destpath = ipAddress + uploadPath;
+        org.apache.hadoop.fs.Path dst = new org.apache.hadoop.fs.Path(destpath);
+        FSDataOutputStream os = null;
+        try {
+            os = fs.create(dst);
+            IOUtils.copy(fileInputStream, os);
+            fs.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return  hash + fileExtName;
+    }
+}
